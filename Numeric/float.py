fe9141f935532d24d9d03d1e3017820821699a5c
R-Mounika12 <91323516+R-Mounika12@users.noreply.github.com>
2023-12-27 09:56:24 +0000

    numeroc operations

---
+++ Numeric/float.py
@@ -0,0 +1,23 @@
+# float variables
+
+a = 4.2
+b= 2.2
+
+sum = a + b
+print("sum ", sum)
+
+diff = a-b
+print("diff ", diff)
+
+mul = a*b
+print("mul ", mul)
+
+div = a/b
+print("div ", div)
+
+mod = a%b
+print("mod (reminder)", mod)
+
+# Rounding
+result5 = round(3.14159265359, 2)  # Rounds to 2 decimal places
+print("Rounded:", result5)
---
+++ Numeric/int.py
@@ -0,0 +1,19 @@
+# int variables
+
+a = 5
+b= 2
+
+sum = a + b
+print("sum ", sum)
+
+diff = a-b
+print("diff ", diff)
+
+mul = a*b
+print("mul ", mul)
+
+div = a/b
+print("div ", div)
+
+mod = a%b
+print("mod (reminder)", mod)
