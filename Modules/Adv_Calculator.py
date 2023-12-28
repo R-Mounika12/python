01e632cf5177b2f0a87143bcc0b291f3d0e94922
R-Mounika12 <91323516+R-Mounika12@users.noreply.github.com>
2023-12-28 06:44:36 +0000

    module impl

---
+++ Modules/Adv_Calculator.py
@@ -0,0 +1,4 @@
+import Calculator_module
+
+print(Calculator_module.addition(10, 5))
+print(Calculator_module)
---
+++ Modules/Calculator_module.py
@@ -0,0 +1,18 @@
+def addition(num1, num2):
+    addition = num1 + num2
+    # print("sum : ", addition)
+    return addition
+
+def subtraction(num1, num2):
+    subtraction = num1 - num2 
+    # print("subtraction : ", subtraction)
+    return subtraction
+
+def multiplication(num1, num2):
+    multiplication = num1*num2
+    # print("multiplication : ", multiplication)
+    return multiplication
+
+addition(10, 5)
+subtraction(10, 5)
+multiplication(10, 5)
