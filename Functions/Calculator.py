03992cd7be6bcd6a9e268070a837e6c28e1a5249
R-Mounika12 <91323516+R-Mounika12@users.noreply.github.com>
2023-12-28 06:17:02 +0000

    function impl

---
+++ Functions/Calculator.py
@@ -0,0 +1,18 @@
+num1 = 2
+num2 = 3
+
+def addition():
+    addition = num1 + num2
+    print("sum : ", addition)
+
+def subtraction():
+    subtraction = num1 - num2 
+    print("subtraction : ", subtraction)
+
+def multiplication():
+    multiplication = num1*num2
+    print("multiplication : ", multiplication)
+
+addition()
+subtraction()
+multiplication()
