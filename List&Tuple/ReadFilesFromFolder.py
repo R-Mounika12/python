cb1be8c71a3341db1503bf6107a1b6278d6eb801
R-Mounika12 <91323516+R-Mounika12@users.noreply.github.com>
2024-01-03 05:24:40 +0000

    impl of reading files from list of folders

---
+++ List&Tuple/ReadFilesFromFolder.py
@@ -0,0 +1,24 @@
+import os
+
+def listOfFilesInFolder(folder):
+    try:
+        files = os.listdir(folder)
+        return files, None
+    except FileNotFoundError:
+        return None, FileNotFoundError
+
+def main():
+    folders = input("Enter the folder names with whitespaces in between ").split()
+    #print(folders)
+
+    for folder in folders:
+        files, errorMessage = listOfFilesInFolder(folder)
+        if files:
+            print(f"files in {folder} ")
+            for file in files:
+                print(file)
+        else:
+            print(f"Error in {folder} ", errorMessage)
+
+main()
+
