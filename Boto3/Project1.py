4b097b3c04de04d1a1c7a2295d7e0644c6d1772e
R-Mounika12 <91323516+R-Mounika12@users.noreply.github.com>
2024-01-06 06:37:04 +0000

    AWS s3 automation using boto3

--- Boto3/Project1.py
+++ Boto3/Project1.py
@@ -3,13 +3,20 @@ import boto3
 
 client = boto3.client('s3')
 
-response = client.create_bucket(
+response_create = client.create_bucket(
     Bucket='mounika-demo-boto3-yt56-111'
 )
+
+print("response_create : ",response_create)
 response1 = client.get_bucket_acl(
     Bucket='mounika-demo-boto3-yt56-111'
 )
-
-print(response1)
+print("response1 : ",response1)
 
 print(response1["ResponseMetadata"]["RequestId"])
+
+response_delete = client.delete_bucket(
+    Bucket='mounika-demo-boto3-yt56-111'
+)
+
+print("response_delete : ", response_delete)
